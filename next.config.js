30be2f68ab1a5a0e90406ccfa01582cbac9dc560
Fredrickadler <13fredrick2001@gmail.com>
2025-04-08 02:05:12 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["nft-mint-mini-app.vercel.app", "i.postimg.cc"], // دامنه‌های تصاویرت
+  },
 };
 
 module.exports = nextConfig;
